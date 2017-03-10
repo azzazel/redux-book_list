ea7ab4b2219757ae42d87567da0b9832cf721850
Goran Jovanovic <goranj@vast.com>
2017-03-10 20:11:56 +0100

    reducer_books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'Javascript: the good parts' },
+    { title: 'Potter Harry' },
+    { title: 'Dark Tower' },
+    { title: 'Master and Margarita' },
+    { title: 'Neuromans er' }    
+  ];
+}
